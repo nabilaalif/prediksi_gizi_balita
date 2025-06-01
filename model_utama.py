db218a05577aa64a2d48d1bf68d101f1624dfa12
nabilaalif <139987833+nabilaalif@users.noreply.github.com>
2025-06-01 20:45:49 +0700

    Update model_utama.py

--- model_utama.py
+++ model_utama.py
@@ -14,10 +14,10 @@ def load_model(algoritma):
 st.markdown("""
     <style>
         body {
-            background: linear-gradient(to right, #e0f7fa, #ffffff);
+            background-color: #bbdefb;
         }
         .stApp {
-            background: linear-gradient(to right, #e0f7fa, #ffffff);
+            background-color: #bbdefb;
         }
         h1, h2, h3, h4, h5, h6 {
             color: #0d47a1;
